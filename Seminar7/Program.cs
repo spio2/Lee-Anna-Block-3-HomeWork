8c47a49191f855a5c11641a59f1ae6ea628e4262
LiAnna <spio@mail.ru>
2023-02-11 12:09:46 +0300

    Поможет с семинаром 9?

---
+++ Seminar7/Program.cs
@@ -0,0 +1,138 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+                                // метод создает 2мер-массив
+/*
+Console.Clear();
+
+double[,] CreateRandom2dArray()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное значение элемента: ");
+    int minVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение элемента: ");
+    int maxVal = Convert.ToInt32(Console.ReadLine());
+    
+    double[,] array = new double[rows, columns];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round(new Random().Next(minVal, maxVal + 1) + new Random().NextDouble(), 1);
+    
+    return array;
+}
+                                // метод показывает 2мер-массив
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Show2dArray(CreateRandom2dArray());
+*/
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> числа с такими индексами в массиве нет
+
+//Console.Clear();
+// void FindElementOf2DArray(double[,] array)
+// {
+//  Console.WriteLine("Now, lets find your element");
+//  Console.WriteLine("Input the number of row:");
+//  int row = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Input the number of column:");
+//  int column = Convert.ToInt32(Console.ReadLine());
+//  if(row < array.GetLength(0) && column < array.GetLength(1))
+//  Console.WriteLine($"Element number {row} in {column} raw is {array[row - 1, column - 1]}");
+
+//  else Console.WriteLine($"There is no such cell in the array");
+// }
+
+
+
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+
+double[] AverageArithmNum(int[,] array)
+{
+    double[] averageNum = new double[array.GetLength(1)];
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double result = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+            result += array[i, j];
+        averageNum[j] = result / array.GetLength(0);
+    }
+    return averageNum;
+}
+
+int[,] CreateRandom2dArray() 
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное значение элемента: ");
+    int minVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение элемента: ");
+    int maxVal = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns]; 
+
+    for(int i = 0; i < rows; i++)              
+        for(int j = 0; j < columns; j++)                      
+            array[i,j] = new Random().Next(minVal, maxVal + 1);
+    
+    return array;
+}
+
+void Show2dArray(int[,] array)                          
+{
+    for(int i = 0; i < array.GetLength(0); i++)   
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");   
+        Console.WriteLine();
+}
+
+void ShowArray(double[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+
+int[,] array = CreateRandom2dArray();
+
+Show2dArray(array);
+ShowArray(AverageArithmNum(array));
---
+++ Seminar9/Program.cs
@@ -0,0 +1,92 @@
+﻿
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+        return;
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+*/
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+/*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int n, int m) 
+{
+    if (n == 0)
+        return m + 1;
+  
+    else
+        if ((n != 0) && (m == 0))
+            return Akkerman(n - 1, 1);
+    
+    else
+        return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+*/
+
